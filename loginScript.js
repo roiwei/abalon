b4054fc2ddccb27ced9ca39cc338ac4b1d51ffe8
roiwei <48522938+roiwei@users.noreply.github.com>
2019-10-31 20:58:34 +0200

    Update loginScript.js

--- loginScript.js
+++ loginScript.js
@@ -19,7 +19,7 @@ function login(){
 	var userEmail = document.getElementById("email_field").value;
 	var userPass = document.getElementById("password_field").value;
 	
-	firebase.auth().signInWithEmailAndPassword(uswrEmail, userPass).catch(function(error) {
+	firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
   // Handle Errors here.
   var errorCode = error.code;
   var errorMessage = error.message;
