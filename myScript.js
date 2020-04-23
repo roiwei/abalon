d856376871edbb28634001f7e89c2dec4c8fca89
roiwei <48522938+roiwei@users.noreply.github.com>
2020-04-23 19:04:21 +0300

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -277,7 +277,7 @@ function save_user(){
 	databaseRef.once('value', function(snapshot) {
 	  snapshot.forEach(function(childSnapshot) {
 	    var childData = childSnapshot.val();
-	if(childData.user_name != ""){
+	if!((childData.user_name === "")){
 		var flag=0;
 		var user_name = document.getElementById('user_name').value; 
 		console.log(user_name);
